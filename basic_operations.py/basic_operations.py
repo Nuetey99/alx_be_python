80ecae2ca3ab805f61096ce70b036670ffd1a413
nuetey99 <nueteyn03@gmail.com>
2024-06-22 18:46:22 +0000

    task 0

---
+++ basic_operations.py/basic_operations.py
@@ -0,0 +1,3 @@
+number1 = 10
+number2 = 5
+print("Addition of ",number1, "and ", number2," is", number1 + number2, "\n", " Subtraction of ",number1, “and “, number2, “is “, number1-number2, "\n", " Multiplication of ",number1, “and “, number2, “is “, number1*number2)
