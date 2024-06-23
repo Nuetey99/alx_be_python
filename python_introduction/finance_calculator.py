8a4acf484302f0cd1a1404ad907b35fca6712d11
nuetey99 <nueteyn03@gmail.com>
2024-06-23 17:04:18 +0000

    added brackets to certain computations

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@ time = 12      #in months
 monthly_income = int(input("Enter your monthly income: "))
 total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = monthly_income-total_monthly_expenses
+monthly_savings = (monthly_income-total_monthly_expenses)
 annual_projected_savings = monthly_savings * time + (monthly_savings * time * annual_interest_rate)
 
 print("Your monthly savings are ", "$",monthly_savings)
