b206a25c660235c006d3280b3b70305501af97fc
nuetey99 <nueteyn03@gmail.com>
2024-06-23 14:41:35 +0000

    task0 - task 4

---
+++ future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age= int(input("How old are you?"))             #take current age of the user
+
+future_age = current_age +27                            #calculate the age of the user in 2050
+
+print("In 2050, you will be ", future_age, "years old")
---
+++ hours_to_seconds.py
@@ -0,0 +1,5 @@
+hours = 2
+
+seconds = 3600 * 2                                      # number of seconds in 2 hours
+
+print("2 hour(s) is ",seconds, " seconds.")
