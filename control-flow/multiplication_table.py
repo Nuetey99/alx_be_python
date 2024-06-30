e042f08eab4c16ad5da109358adc9c7a0f648f07
nuetey99 <nueteyn03@gmail.com>
2024-06-30 16:05:09 +0000

    task 2

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number to see its multiplication table:"))
+
+for iterator in range(1,11):
+    product = number * iterator
+    print (number, "*", iterator, " = ", product)
