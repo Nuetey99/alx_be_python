312c7ff5cc1cd7ce3750bd0eba3ea4a8357a0e3b
nuetey99 <nueteyn03@gmail.com>
2024-06-30 21:10:13 +0000

    task 2

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,21 +4,22 @@ operation_type = input("Choose the operation (+, -, *, /):")
 
 match operation_type:
         case "+":
-            result = num1+num2
+            result = num1 + num2
             print ("The result is ",result)
         
         case "-":
-            result = num1-num2
+            result = num1 - num2
             print("The result is ", result) 
 
         case "*":
-            result = num1*num2
+            result = num1 * num2
             print("The result is ",result) 
 
         case "/":
+            result = num1 / num2
             if num1 or num2 == 0:
                 print("Cannot divide by zero.")
 
-            result = num1/num2
+            result = num1 / num2
             print("The result is ", result) 
 
