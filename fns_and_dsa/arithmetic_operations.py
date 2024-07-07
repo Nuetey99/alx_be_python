fdddd7f23a6df1818ecaa46b03f5eed4646f97c1
nuetey99 <nueteyn03@gmail.com>
2024-07-07 23:13:51 +0000

    task 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,16 @@
 def perform_operation(num1, num2, operation):
-    if operation=="add":
-        result=num1+num2
-    elif operation=="subtract":
-        result=num1-num2
-    elif operation=="multiplication":
-         result=num1*num2
-    elif operation=="division":
+
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiplication":
+         result = num1 * num2
+    elif operation == "division":
         if num1 or num2 == 0:
             print ("Division error!")
         else :
-                result=num1/num2
+                result = num1 / num2
     return result
          
        
