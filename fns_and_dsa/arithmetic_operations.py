3d2898f486a6cee9d6b6aaf9efe64a00cee845a9
nuetey99 <nueteyn03@gmail.com>
2024-07-07 22:50:00 +0000

    task 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,17 @@
 def perform_operation(num1, num2, operation):
      match operation:
-          case "add":
+        case "add":
                return num1+num2
-          case "subtract":
+        case "subtract":
               return num1-num2
-          case "multiplication":
+        case "multiplication":
                return num1*num2
-          case "division" :
+        case "division" :
                if num1 or num2 == 0:
                     print ("Division error!")
                else :
                    return num1/num2
-          case _:
+        case _:
                print("An error occurred!")
 
 
