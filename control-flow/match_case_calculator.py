bdb358c8309df94be07ef2099196e6c1ccf765c8
nuetey99 <nueteyn03@gmail.com>
2024-07-07 22:58:33 +0000

    task 0

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -22,3 +22,18 @@ match operation_type:
             result = num1 / num2
             print("The result is ", result) 
 
+
+ match operation: 
+        case "add":
+               return num1+num2
+        case "subtract":
+              return num1-num2
+        case "multiplication":
+               return num1*num2
+        case "division" :
+               if num1 or num2 == 0:
+                    print ("Division error!")
+               else :
+                   return num1/num2
+        case _:
+               print("An error occurred!")
--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,16 @@
 def perform_operation(num1, num2, operation):
-     match operation:
-        case "add":
-               return num1+num2
-        case "subtract":
-              return num1-num2
-        case "multiplication":
-               return num1*num2
-        case "division" :
-               if num1 or num2 == 0:
-                    print ("Division error!")
-               else :
-                   return num1/num2
-        case _:
-               print("An error occurred!")
-
-
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "division":
+        if num1 or num2 == 0:
+            print ("Division error!")
+        else :
+                result = num1/num2
+    elif operation == "multiplication":
+         result = num1*num2
+         
+       
          
                    
