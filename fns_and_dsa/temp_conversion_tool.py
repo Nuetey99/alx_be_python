eff8f88a1d194397e18abbfab2fa97ed1f18eba1
nuetey99 <nueteyn03@gmail.com>
2024-07-08 00:59:42 +0000

    task 3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 #Definition of global conversion factors
-global FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-global CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR =5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR =9/5
 
 def convert_to_celsius(fahrenheit):
     return(fahrenheit-32)* FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -10,8 +10,8 @@ def convert_to_fahrenheit(celsius):
 
 def main():
     try:
-         temperature = float(input("Enter the temperature"))
-         unit = input("Celsius/Fahrenheit")
+         temperature = float(input("Enter the temperature: "))
+         unit = input("C[Celsius] / F[Fahrenheit]: ")
 
          if unit == "C":
              converted_temp = convert_to_fahrenheit(temperature)
@@ -22,7 +22,8 @@ def main():
          else:
              print("Invalid temperature unit entered.")
     except ValueError:
-        print ("Invalid temperature. Please enter a numeric value")
+        print("Invalid temperature. Please enter a numeric value")
 
-        if __name__ == "__main__":
+
+if __name__ == "__main__":
             main()
