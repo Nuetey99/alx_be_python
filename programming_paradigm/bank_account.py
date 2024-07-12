a002496fa71dce9b2652ccba56dfb1d76a943711
nuetey99 <nueteyn03@gmail.com>
2024-07-12 10:54:59 +0000

    task 0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__ (self, account_balance):
-        self.current_balance = account_balance
+    def __init__ (self, initial_balance = 0.0):
+        self.current_balance = initial_balance
         
     
     def deposit(self, amount):
@@ -10,8 +10,9 @@ class BankAccount:
     def withdraw(self,amount):
         if self.current_balance >= amount:
             self.current_balance = amount - self.current_balance
+            return True
         else:
-            raise Exception("Insufficient funds")
+            return False
 
     def display_balance(self):
         return print(f"${self.current_balance:.2f} is your current balance")
